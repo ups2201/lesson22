ee3197a6ff1b133adc40086254a6f0cd6bfa7172
ups2201 <ups2201@gmail.com>
2024-07-13 18:26:03 +0300

    Добавлены реализации календаря

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { RemoteStorage } from "./calendar/RemoteStorage";
 import { LocalStorage } from "./calendar/LocalStorage";
+import {Task} from "./calendar/Task";
+import {Calendar} from "./calendar/Calendar";
 
-const remoteStorage: RemoteStorage = new RemoteStorage();
-remoteStorage.getItem("day1").then((result) => console.log(result));
-remoteStorage
-  .setItem("day2", { test3: 3 })
-  .then(() => console.log("Данные успешно сохранились в firebase"));
+const task1 = new Task(1, new Date(Date.now()), new Set<string>(["tag1, tag2"]), "OPEN", "Первая задача");
 
-const localStorage: LocalStorage = new LocalStorage();
-localStorage.setItem("day2", { test3: 3 });
-localStorage.getItem("day2").then((result) => console.log(result));
+const remoteStorage: RemoteStorage = new RemoteStorage("remoteStorage/tasks/");
+const calendar1 = new Calendar(document.querySelector('body'), remoteStorage);
+calendar1.addTask(task1);
+calendar1.getTask("1").then((result) => console.log(result));
+
+const localStorage: LocalStorage = new LocalStorage("localStorage/tasks/");
+const calendar2 = new Calendar(document.querySelector('body'), localStorage);
+
+calendar2.addTask(task1);
+calendar2.getTask("1").then((result) => console.log(result));
